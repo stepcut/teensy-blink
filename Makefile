3af43378d21b702b4a9418ffe29ecaa5934d9992
stepcut <jeremy@n-heptane.com>
2014-10-15 18:37:21 -0500

    minor doc tweaks

--- Makefile
+++ Makefile
@@ -4,4 +4,11 @@ ARDUINO_DIR  = /home/stepcut/n-heptane/projects/arduino/arduino-1.0.6
 TARGET = blink.c
 ARDUINO_LIBS =
 
+# modify to point to where you have checked out Arduino-Makefile
+#
+# Use:
+#
+#  git clone https://github.com/sudar/Arduino-Makefile
+#
+# to get the source for Arduino-Makefile
 include /home/stepcut/n-heptane/projects/arduino/Arduino-Makefile/Teensy.mk
--- blink.c
+++ blink.c
@@ -2,17 +2,17 @@
  * http://www.pjrc.com/teensy/
  * Copyright (c) 2008 PJRC.COM, LLC
  * Copyright (c) 2014 Jeremy Shaw
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -106,8 +106,8 @@ void morse_character(char c)
         print("Opps, unsupported character: ");
         pchar(c);
         print("\n");
-        return; 
-    } 
+        return;
+    }
     print("Char ");
     pchar(c);
     pchar(':');
